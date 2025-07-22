b49877c9638af4f95eb17482364de065764d7f1b
Pratiksha Nikam <pratikshanikam966@gmail.com>
2025-07-22 06:56:21 -0700

    Right-Aligned Half Pyramid Pattern (with spaces)

---
+++ Pattern5.java
@@ -0,0 +1,26 @@
+
+//*
+//* *
+//* * *
+//* * * *
+//* * * * *
+//* * * * * *
+
+import java.util.Scanner;
+public class Pattern5{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enetr your number :");
+    int num = sc.nextInt();
+     for(int i = 1; i<=num; i++){
+        for(int j=1; j<=num-i; j++){
+            System.out.print("  ");
+        }
+        for(int k = 1; k<=i; k++){
+            System.out.print("*");
+        }
+        System.out.println();
+     }
+
+    }
+}
